0c7af5cd8d81f22ee7ed07c0f6351a86e80ed533
Manolya Tamimi <tamimimanolya@gmail.com>
2025-03-23 10:22:34 +0200

    2.using navLink

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,29 @@
 import "./App.css";
-import { useState } from "react";
 import { Home, About, SignIn } from "./pages/index"
 import Header from "./Components/Header";
 import { BrowserRouter, Routes, Route } from "react-router"
 
 export default function App() {
-	const [page, setPage] = useState("home");
+	// const [page, setPage] = useState("home");
 
 	return (
 		<div className="app-wrapper">
-			<Header setPage={setPage} />
-			<div className="content-container">
-		
 			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/about" element={<About />} />
-				</Routes>
-				<Routes>
-					<Route path="/signin" element={<SignIn />} />
-				</Routes>
+				<Header /*setPage={setPage}*/ />
+				<div className="content-container">
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/about" element={<About />} />
+						<Route path="/signin" element={<SignIn />} />
+					</Routes>
+
+				</div>
 			</BrowserRouter>
-			</div>
-			
-			{/* <div className="content-container">
-				{page === "home" ? <Home /> : page === "about" ? <About /> : <SignIn />}
-			</div> */}
-		</div>
-	);
+		</div >
+	)
 }
 
 /*
-	npm i react-router
-	imported <BrowserRouter />, Routes, and Route
-	each project should havve 1 browser router
-	The <Routes /> can be used to group <Routes /> Together
-	Note that you can nest routes inside each other
-
-	Now changing the path in the browser renders the appropirate content
-	but the header no longer works, we need a mechanism to navigate (clicking the buttons should change the path)
-*/
-
+	realized we need to have the header inside the router
+	and   
+ */
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,17 @@
-const Header = ({ setPage }: { setPage: React.Dispatch<React.SetStateAction<string>> }) => {
+import { NavLink } from "react-router";
+
+const Header = (/*{ setPage }: { setPage: React.Dispatch<React.SetStateAction<string>> }*/) => {
+
     return (
         <nav className="navbar">
-            <button onClick={() => setPage("home")} className="nav-button">Home</button>
-            <button onClick={() => setPage("about")} className="nav-button">About</button>
-            <button onClick={() => setPage("signin")} className="nav-button">Sign In</button>
+            <NavLink className="nav-button" to="/">Home</NavLink>
+            <NavLink className="nav-button" to={{ pathname: "/about" }}>About</NavLink>
+            <NavLink className="nav-button" to={{ pathname: "/signin" }}>Sign In</NavLink>
+
+
+            {/* <button onClick={() => setPage("home")} className="nav-button">Home</button> */}
+            {/* <button onClick={() => setPage("about")} className="nav-button">About</button> */}
+            {/* <button onClick={() => setPage("signin")} className="nav-button">Sign In</button> */}
         </nav>
     )
 }
